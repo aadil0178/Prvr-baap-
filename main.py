dfb6bee34298d664e775f15d9b66e8f5e87ccb8b
aadil0178 <jinwaji9@gmail.com>
2026-07-28 08:32:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ SIGNATURE = "༺AADIL अब्बू ☽༻"
 SIGNATURE_CHANCE = 0.15 
 
 def get_payload():
-    base_text = "SURIYA OR US K TATTE ᴛʀʏ. ᴍᴀ ғʟᴏᴡᴇʀ."
+    base_text = "ANSH -VERRU-GUN-RAKSHIT ᴛʀʏ. ᴍᴀ ғʟᴏᴡᴇʀ."
     fire_part = "ʏᴀ ғɪʀᴇ 🔥??"
     flowers = ["🌸", "🌹", "🌺", "🌻", "🌼", "🌷"]
     line = f"{base_text} {random.choice(flowers)} {fire_part}"
